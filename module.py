2252e7982b52882a0d562005d811883536550e11
Shameiz Rangwala <srangwal@edu.uwaterloo.ca>
2017-03-03 23:24:00 -0500

    new

--- module.py
+++ module.py
@@ -1,2 +1,7 @@
 import clientpy2
-clientpy2.run("sgt", "bl00mb3rgh4CK","STATUS")
+a=1
+while a == 1:
+    clientpy2.run("a", "a","ACCELERATE 0 0.5")
+    clientpy2.run("a", "a","ACCELERATE 3.14 1")
+    clientpy2.run("a", "a","ACCELERATE 1.57 0.5")
+    clientpy2.run("a", "a","ACCELERATE 4.71 1")
